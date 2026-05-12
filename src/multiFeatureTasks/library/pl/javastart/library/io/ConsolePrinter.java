51ce5de1dc1429980f8b611e9d9a2889f6476036
Karola <wolnikkarolina1@gmail.com>
2026-05-12 22:36:41 +0200

    Refactor display methods using Stream API

--- src/multiFeatureTasks/library/pl/javastart/library/io/ConsolePrinter.java
+++ src/multiFeatureTasks/library/pl/javastart/library/io/ConsolePrinter.java
@@ -1,42 +1,35 @@
 package multiFeatureTasks.library.pl.javastart.library.io;
 
-import multiFeatureTasks.library.pl.javastart.library.model.Book;
-import multiFeatureTasks.library.pl.javastart.library.model.LibraryUser;
-import multiFeatureTasks.library.pl.javastart.library.model.Magazine;
-import multiFeatureTasks.library.pl.javastart.library.model.Publication;
+import multiFeatureTasks.library.pl.javastart.library.model.*;
 
 import java.util.Collection;
 
 public class ConsolePrinter {
 
     public void printUser(Collection<LibraryUser> users) {
-        for (LibraryUser user : users) {
-            printLine(user.toString());
-        }
+        users.stream()
+                .map(User::toString)
+                .forEach(this::printLine);
     }
 
     public void printBooks(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book) {
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if ( counter == 0) {
+        long count = publications.stream()
+                .filter(p -> p instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if ( count == 0) {
             printLine("Brak książek w bibliotece");
         }
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Magazine) {
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if ( counter == 0) {
+        long count = publications.stream()
+                .filter(p -> p instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if ( count == 0) {
             printLine("Brak książek w bibliotece");
         }
     }
